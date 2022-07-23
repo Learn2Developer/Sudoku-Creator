a55976b262eb0ae4d63c8adfc23d0666f5bcb0ea
Jesse Wagner <jwag1234@hotmail.com>
2022-07-23 01:20:28 -0300

    Created random board logic

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,23 +1,19 @@
-import random
+from random import sample
 board = []
 game_still_going = True
 square = []
 col = []
-
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 """So we need to make the logic for making the 2D grid, then create a board for the game, 
 then, given a board state, how does the game know which squares to fill in?"""
 
-
-for each in range(0, 9):
-    board.append("x" * 9)
-
 def print_board(board):
+
+def make_board():
     x = 0
-    for i in board:
-        print("|" + "|".join(i) + "|")
+    while x < 9:
+        board.append(sample(num_list, 9))
         x += 1
-        if x % 3 == 0:
-            print("___________________")
 
 def check_row(row):
     while "123456789" not in row:
@@ -32,7 +28,9 @@ def check_square(square):
 def check_col(col):
     while "12345789" not in col:
         #col not complete
+        #possibly check for columns by zipping lists and checking that each index contains 1-9.
         pass
 
-
+make_board()
 print_board(board)
+print(sample(num_list, 9))
